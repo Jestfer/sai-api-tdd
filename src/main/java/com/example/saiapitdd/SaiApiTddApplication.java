484e82bddb38e190e178fc5d9d980819c5a94895
Josué Estévez Fernández <jestfer6@gmail.com>
2018-07-04 15:23:12 +0100

    first commit

---
+++ src/main/java/com/example/saiapitdd/SaiApiTddApplication.java
@@ -0,0 +1,12 @@
+package com.example.saiapitdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaiApiTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaiApiTddApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/saiapitdd/SaiApiTddApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.saiapitdd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SaiApiTddApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
